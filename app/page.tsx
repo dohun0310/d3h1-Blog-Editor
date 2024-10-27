26814ab800df26b103a63aaa85057513dcd67967
dohun0310 <dohun0310@naver.com>
2024-10-27 17:38:21 +0900

    edit main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,33 @@
+import Link from "next/link";
 import styles from "./page.module.css";
-
-import Sidebar from "@/components/Sidebar";
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <Sidebar />
-    </main>
+    <div className={styles.list}>
+      <Link className={styles.status} href="/">
+        <p>Frontend 상태</p>
+        <div className={styles.one} />
+      </Link>
+      <Link className={styles.status} href="/">
+        <p>Backend 상태</p>
+        <div className={styles.one} />
+      </Link>
+      <Link className={styles.status} href="/">
+        <p>배포 상태</p>
+        <div className={styles.one} />
+      </Link>
+      <Link className={styles.status} href="/">
+        <p>GitHub 상태</p>
+        <div className={styles.one} />
+      </Link>
+      <Link className={styles.status} href="/">
+        <p>Vercel 상태</p>
+        <div className={styles.one} />
+      </Link>
+      <Link className={styles.status} href="/">
+        <p>Cloudflare 상태</p>
+        <div className={styles.one} />
+      </Link>
+    </div>
   );
 }
