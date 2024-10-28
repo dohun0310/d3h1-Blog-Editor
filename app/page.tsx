8956a6610238ec8e1dfad326f9f2dd7089fb6559
dohun0310 <dohun0310@naver.com>
2024-10-28 09:41:02 +0900

    add status indicator fetching functions and hook

--- app/page.tsx
+++ app/page.tsx
@@ -1,37 +1,35 @@
-"use client"
-
 import Link from "next/link";
 import styles from "./page.module.css";
 import useStatuses from "@/tools/status";
 
-export default function Home() {
-  const { frontend, cloudflare, github, vercel } = useStatuses();
+export default async function Home() {
+  const statuses = await useStatuses();
 
   return (
     <div className={styles.list}>
       <Link className={styles.status} href="https://blog.d3h1.com">
         <p>Frontend 상태</p>
-        <div className={styles[frontend]} />
+        <div className={styles[statuses.frontend]} />
       </Link>
       <Link className={styles.status} href="/">
         <p>Backend 상태</p>
-        <div className={styles.one} />
+        <div className={styles.none} />
       </Link>
       <Link className={styles.status} href="/">
         <p>배포 상태</p>
-        <div className={styles.one} />
+        <div className={styles.none} />
       </Link>
-      <Link className={styles.status} href="/">
+      <Link className={styles.status} href="https://www.githubstatus.com">
         <p>GitHub 상태</p>
-        <div className={styles[github]} />
+        <div className={styles[statuses.github]} />
       </Link>
-      <Link className={styles.status} href="/">
+      <Link className={styles.status} href="https://www.vercel-status.com">
         <p>Vercel 상태</p>
-        <div className={styles[vercel]} />
+        <div className={styles[statuses.vercel]} />
       </Link>
-      <Link className={styles.status} href="/">
+      <Link className={styles.status} href="https://www.cloudflarestatus.com">
         <p>Cloudflare 상태</p>
-        <div className={styles[cloudflare]} />
+        <div className={styles[statuses.cloudflare]} />
       </Link>
     </div>
   );
