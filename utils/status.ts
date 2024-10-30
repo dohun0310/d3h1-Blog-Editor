1f7a3ac9e97e9bef04fa0c27e5213de517f40d85
dohun0310 <dohun0310@naver.com>
2024-10-30 15:16:49 +0900

    add Vercel deployment status fetching function and integrate into status hook

--- utils/status.ts
+++ utils/status.ts
@@ -1,3 +1,5 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+
 async function fetchStatus(url: string) {
   const response = await fetch(url);
   const data = await response.json();
@@ -19,9 +21,32 @@ async function fetchHttpStatus(url: string) {
   return response.status === 200 ? "operational" : "error";
 }
 
+export async function fetchVercelDeployStatus(name: string) {
+  const response = await fetch(`https://api.vercel.com/v3/deployments/?app=${name}`, {
+    "headers": {
+      "Authorization": `Bearer ${process.env.VERCEL_TOKEN}`
+    },
+    "method": "get"
+  });
+  const data = await response.json();
+
+  if (data.deployments[0].state === "READY") {
+    return "operational";
+  } else if (data.deployments[0].state === "ERROR") {
+    return "error";
+  } else if (data.deployments[0].state === "CANCELED") {
+    return "warning";
+  } else if (data.deployments[0].state === "BUILDING") {
+    return "running";
+  } else {
+    return "none";
+  }
+}
+
 export default async function useStatuses() {
   const statuses = {
     frontend: await fetchHttpStatus("https://blog.d3h1.com"),
+    frontendDeploy: await fetchVercelDeployStatus("d3h1-blog"),
     cloudflare: await fetchStatus("https://www.cloudflarestatus.com/api/v2/status.json"),
     github: await fetchStatus("https://www.githubstatus.com/api/v2/status.json"),
     vercel: await fetchStatus("https://www.vercel-status.com/api/v2/status.json"),
